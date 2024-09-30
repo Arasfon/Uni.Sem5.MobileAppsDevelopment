7c3777b9be6f26d4a564aa4b678b04b583ea3046
Egor Boyarkin <egrsfn@gmail.com>
2024-10-01 01:14:22 +0300

    Add Montserrat font and common typography styles

--- app/src/main/java/com/arasfon/uni/mobileappsdevelopment/common/theme/Type.kt
+++ app/src/main/java/com/arasfon/uni/mobileappsdevelopment/common/theme/Type.kt
@@ -2,33 +2,61 @@ package com.arasfon.uni.mobileappsdevelopment.common.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.arasfon.uni.mobileappsdevelopment.R
 
-// Set of Material typography styles to start with
+val montserratFontFamily = FontFamily(
+        Font(R.font.montserrat_thin, FontWeight.ExtraLight),
+        Font(R.font.montserrat_thinitalic, FontWeight.ExtraLight, FontStyle.Italic),
+
+        Font(R.font.montserrat_extralight, FontWeight.ExtraLight),
+        Font(R.font.montserrat_extralightitalic, FontWeight.ExtraLight, FontStyle.Italic),
+
+        Font(R.font.montserrat_light, FontWeight.Light),
+        Font(R.font.montserrat_lightitalic, FontWeight.Light, FontStyle.Italic),
+
+        Font(R.font.montserrat_regular, FontWeight.Normal),
+        Font(R.font.montserrat_italic, FontWeight.Normal, FontStyle.Italic),
+
+        Font(R.font.montserrat_medium, FontWeight.Medium),
+        Font(R.font.montserrat_mediumitalic, FontWeight.Medium, FontStyle.Italic),
+
+        Font(R.font.montserrat_semibold, FontWeight.SemiBold),
+        Font(R.font.montserrat_semibolditalic, FontWeight.SemiBold, FontStyle.Italic),
+
+        Font(R.font.montserrat_bold, FontWeight.Bold),
+        Font(R.font.montserrat_bolditalic, FontWeight.Bold, FontStyle.Italic),
+
+        Font(R.font.montserrat_extrabold, FontWeight.ExtraBold),
+        Font(R.font.montserrat_extrabolditalic, FontWeight.ExtraBold, FontStyle.Italic),
+
+        Font(R.font.montserrat_black, FontWeight.Black),
+        Font(R.font.montserrat_blackitalic, FontWeight.Black, FontStyle.Italic),
+    )
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = montserratFontFamily,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 16.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    titleLarge = TextStyle(
+        fontFamily = montserratFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 22.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = montserratFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = montserratFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
     )
-    */
 )

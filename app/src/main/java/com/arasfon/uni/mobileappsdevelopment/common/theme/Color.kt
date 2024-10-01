92e7bd400fc81400c1827959aed0447f9c8b8f1f
Egor Boyarkin <egrsfn@gmail.com>
2024-10-01 22:58:00 +0300

    Configure color palette and enforce light theme

--- app/src/main/java/com/arasfon/uni/mobileappsdevelopment/common/theme/Color.kt
+++ app/src/main/java/com/arasfon/uni/mobileappsdevelopment/common/theme/Color.kt
@@ -2,10 +2,15 @@ package com.arasfon.uni.mobileappsdevelopment.common.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val DarkText = Color(0xFF181818)
+val LightText = Color(0xFFF5F5F5)
+val Background = Color(0xFFF5F5F5)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Accent = Color(0xFF2A1246)
+
+val ContainerBackground = Color(0xFFF9F5FF)
+
+val OutlineContainerStroke = Color(0xFF181818)
+val OutlineContainerBackground = Color(0xFFFFFFFF)
+
+val Error = Color(0xFFF04438)

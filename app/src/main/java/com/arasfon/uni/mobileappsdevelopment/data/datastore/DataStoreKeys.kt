cfcafc9881092113b37b10bf47bdf7258c7255b6
Egor Boyarkin <egrsfn@gmail.com>
2024-10-10 21:20:48 +0300

    Add DataStore key to track onboarding completion

---
+++ app/src/main/java/com/arasfon/uni/mobileappsdevelopment/data/datastore/DataStoreKeys.kt
@@ -0,0 +1,7 @@
+package com.arasfon.uni.mobileappsdevelopment.data.datastore
+
+import androidx.datastore.preferences.core.booleanPreferencesKey
+
+object DataStoreKeys {
+    val IS_ONBOARDING_COMPLETE = booleanPreferencesKey("is_onboarding_complete")
+}

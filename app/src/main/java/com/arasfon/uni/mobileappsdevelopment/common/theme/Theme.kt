f13498e59b8541cd8e5d688fc1c7cfc7ffcceb59
Egor Boyarkin <egrsfn@gmail.com>
2024-10-10 20:09:12 +0300

    Fix status bar color flicker on app launch

--- app/src/main/java/com/arasfon/uni/mobileappsdevelopment/common/theme/Theme.kt
+++ app/src/main/java/com/arasfon/uni/mobileappsdevelopment/common/theme/Theme.kt
@@ -1,9 +1,13 @@
 package com.arasfon.uni.mobileappsdevelopment.common.theme
 
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val LightColorScheme = lightColorScheme(
     primary = Accent,
@@ -21,6 +25,16 @@ fun DriveNextTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            WindowCompat
+                .getInsetsController(window, view)
+                .isAppearanceLightStatusBars = true
+        }
+    }
+
     MaterialTheme(
         colorScheme = LightColorScheme,
         typography = Typography,

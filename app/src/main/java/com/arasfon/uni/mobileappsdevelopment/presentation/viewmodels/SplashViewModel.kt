56b16b68aed08fa0a0c692e90b2bdc3b9c26be2e
Egor Boyarkin <egrsfn@gmail.com>
2024-10-10 21:31:57 +0300

    Add view models for OnboardingScreen and SplashScreen

---
+++ app/src/main/java/com/arasfon/uni/mobileappsdevelopment/presentation/viewmodels/SplashViewModel.kt
@@ -0,0 +1,27 @@
+package com.arasfon.uni.mobileappsdevelopment.presentation.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.arasfon.uni.mobileappsdevelopment.domain.usecases.GetIsOnboardingCompleteUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SplashViewModel @Inject constructor(
+    private val getIsOnboardingCompleteUseCase: GetIsOnboardingCompleteUseCase
+) : ViewModel() {
+    private val _isOnboardingComplete = MutableStateFlow(false)
+    val isOnboardingComplete: StateFlow<Boolean> = _isOnboardingComplete.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            getIsOnboardingCompleteUseCase().collect { isOnboardingComplete ->
+                _isOnboardingComplete.value = isOnboardingComplete
+            }
+        }
+    }
+}

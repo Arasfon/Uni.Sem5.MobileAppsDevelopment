3f099ba6c7f35c39c2ca3ff9b71ab1753b2c85ef
Egor Boyarkin <egrsfn@gmail.com>
2024-10-11 12:48:11 +0300

    Unify primary constructor style

--- app/src/main/java/com/arasfon/uni/sem5/drivenext/presentation/navigation/Screen.kt
+++ app/src/main/java/com/arasfon/uni/sem5/drivenext/presentation/navigation/Screen.kt
@@ -1,6 +1,8 @@
 package com.arasfon.uni.sem5.drivenext.presentation.navigation
 
-sealed class Screen(val route: String) {
+sealed class Screen(
+    val route: String
+) {
     data object Splash : Screen("splash")
     data object Onboarding : Screen("onboarding")
     data object Main : Screen("main")

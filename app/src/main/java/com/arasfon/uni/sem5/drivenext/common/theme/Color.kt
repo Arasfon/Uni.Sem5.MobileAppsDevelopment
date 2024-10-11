04ce9c3f604953ee2a31dd5ffb2c4baac62dc321
Egor Boyarkin <egrsfn@gmail.com>
2024-10-11 03:24:30 +0300

    Fix color names and reference background color for OutlinedContainerBackground

--- app/src/main/java/com/arasfon/uni/sem5/drivenext/common/theme/Color.kt
+++ app/src/main/java/com/arasfon/uni/sem5/drivenext/common/theme/Color.kt
@@ -10,7 +10,7 @@ val Accent = Color(0xFF2A1246)
 
 val ContainerBackground = Color(0xFFF9F5FF)
 
-val OutlineContainerStroke = Color(0xFF181818)
-val OutlineContainerBackground = Color(0xFFFFFFFF)
+val OutlinedContainerStroke = Color(0xFF181818)
+val OutlinedContainerBackground = Background
 
 val Error = Color(0xFFF04438)
